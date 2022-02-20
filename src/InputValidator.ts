8c14ea7774b62fa5bf3537d75431b71aa9e23164
DanielSerio <dserio.d@gmail.com>
2022-02-20 14:02:59 -0500

    added 3rd position flag check

--- src/InputValidator.ts
+++ src/InputValidator.ts
@@ -1,14 +1,37 @@
+import { CommandFlag } from './CommandFlag'
+
 export class InputValidator {
-  public static isValid = (inputArgs: string[]): boolean => {
+  private _flags: CommandFlag[]
+  constructor (projectFlags: CommandFlag[]) {
+    this._flags = projectFlags
+  }
+
+  public get projectFlagsArray (): string[] {
+    const values: string[] = []
+
+    for (let i = 0; i < this._flags.length; i += 1) {
+      values.push(this._flags[i].full)
+      values.push(this._flags[i].short)
+    }
+
+    return values
+  }
+
+  public isValid = (inputArgs: string[]): boolean => {
     if (inputArgs.length < 3) return false
     // Check if user input project name is valid
     if (!this.isUrlSafe(inputArgs[2])) return false
-    // TODO: check if 2nd user input is a valid project-type tag
+    if (!this.hasValidFlagFormat(inputArgs[3])) return false
+    if (this.projectFlagsArray.includes(inputArgs[3])) return false
     // TODO: if there are more userInputs, check to see if they are valid tags.
     return true
   }
 
-  private static isUrlSafe = (value: string): boolean => {
+  private isUrlSafe = (value: string): boolean => {
     return /^([a-z]|-|_|\d)+$/.test(value)
+  }
+
+  private hasValidFlagFormat = (value: string): boolean => {
+    return /^(--\w+)|(-\w{1,2})$/.test(value)
   }
 }
